15f3b270352aafc8939e7342b6dab2c4a7ad6517
Levi Duckworth <leviduck97@gmail.com>
2024-02-28 18:23:51 -0500

    Started over with a new backend and properly tested it

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using backend.Migrations;
+using backend.Repositories;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSqlite<LocationDbContext>("Data Source=SqlDatabase.db");
+builder.Services.AddScoped<ILocationRepository, LocationRepository>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
